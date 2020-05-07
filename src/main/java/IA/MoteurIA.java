34f06d39b01d6b8779c675fe029cf8eabcf2603f
Nada <Impetuoson@gmail.com>
2020-05-07 19:00:10 +0200

    Presque fini le moteur

--- src/main/java/IA/MoteurIA.java
+++ src/main/java/IA/MoteurIA.java
@@ -1,5 +1,4 @@
 package IA;
-
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.ServerSocket;
@@ -9,51 +8,13 @@ import java.util.Scanner;
 
 public class MoteurIA {
 
-	int tab[][] = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
-
-	void placement(int l, int c, int p, int t){
-		switch(p){
-			case 1 :
-				tab[l][c] = t;
-			break;
-			case 2 :
-				tab[l][c] = -t;
-			break;
-			default:
-		}
-	}
-
-
 	public static void main (String[] args) {
+		int plateau[][] = {{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1},{-1,-1,-1,-1}};
+		int reserve[][]= {{2,2,2,2},{2,2,2,2}};
 
-
-
-		for(int i =0 ; i < 4; i++){
-			for(int j = 0; j < 4;j++){
-				
-			}
-		}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
 		int port = 0;
 		String addr = "";
-		int arret = 0;
+		int nbPartie = 0;
 		if(args.length != 2) {
 			System.out.println("Usage : adresse port");
 		}
@@ -63,37 +24,53 @@ public class MoteurIA {
 			Socket sock = new Socket(addr,port);
 			DataInputStream DIS = new DataInputStream(sock.getInputStream());
 			DataOutputStream DOS = new DataOutputStream(sock.getOutputStream());
-			int op1 = -1;
-			int op2 = -1;
-			int op3 = -1;
-			char operande = 'o';
-			Scanner sc = new Scanner(System.in);
-			while (arret <= 1) {
-				System.out.println("Premier operande");
-				op1 = sc.nextInt();
-				System.out.println("Operateur");
-				operande = sc.next().charAt(0);
-				System.out.println("Seconde operande");
-				op2 = sc.nextInt();
-				int result = -1;
-				DOS.writeInt(op1);
-				op3 = (int) operande;
-				DOS.writeInt(op3);
-				DOS.writeInt(op2);
+			while (nbPartie < 2) {
 				result = DIS.readInt();
-				System.out.println("Réception du calcul");
-				System.out.println(op1 + " " + operande + " " + op2 + " = " + result);
-				System.out.println("Continuer les calculs ? (0/1)");
-				arret = sc.nextInt();
-				DOS.writeInt(arret);
-				if (arret > 0) {		
-					sock.close();
-					break;
+				if(result>1000){
+					nbPartie++;
+					for(int i = 0; i< 4;i++ ){
+						for(int j =0; j<4;j++){
+							plateau[j][i] = -1;
+							if(j<2)
+								reserve[j][i] = 2;
+						}
+					}
+					continue;
 				}
+				int type = result%10;
+				result/=10;
+				int colonne = result%10;
+				result/=10;
+				int ligne =  result;
+
+					//ask prolog
+
+					plateau[ligne][colonne] = type;
+					reserve[1][type]--;
+
+					int reponse = 0;
+					//compute response prolog
+
+					reserve[0][type]--;
+
+					DOS.writeInt(reponse);
+
+					int verification = DIS.readInt();
+					if(verification == 0){
+						nbPartie++;
+						for(int i = 0; i< 4;i++ ){
+							for(int j =0; j<4;j++){
+								plateau[j][i] = -1;
+								if(j<2)
+									reserve[j][i] = 2;
+							}
+						}
+					}
 			}
+			sock.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}*/
+	}
 }
 
