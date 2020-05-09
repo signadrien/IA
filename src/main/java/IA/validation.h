00901b6349137b6fb630b948aa910b3b1f08cc32
Nada <Impetuoson@gmail.com>
2020-05-09 16:43:45 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/IA/validation.h
+++ src/main/java/IA/validation.h
@@ -8,23 +8,24 @@
  *  resume : entete pour la validation des coups
  *
  *  date :      20 / 02 / 20
- *  modifie :
+ *  modifie : 
  ***********************************************************
  */
 
 #ifndef _VALIDATION_H
 #define _VALIDATION_H
-#include "ProtoQuantik.h"
+
 /* Validation d'un coup :
- * parametres :
- *    le numero du joueur courant : 1 (le premier qui a commence a jouer) ou
+ * parametres : 
+ *    le numero du joueur courant : 1 (le premier qui a commence a jouer) ou 
  *                                  2 (le deuxieme)
  *    le coup (TCoupReq)
- * resultat : type bool (coup valide ou non)
+ * resultat : type bool (coup valide ou non) 
  *            propriete du coup  (GAGNE, PERDU, NUL - le coup rend le joueur gagnant, perdant ou la partie est nulle, ou CONT si aucune des autres)
 */
 
-bool validationCoup(int joueur, TCoupReq coup, TPropCoup* propCoup);
+
+bool validationCoup(int joueur, TCoupReq coup, TPropCoup *propCoup);
 /* Initialiser une partie : au cas d'une fin de partie (gagnee, perdue ou nulle), informer du demarrage d'une nouvelle partie */
 void initialiserPartie();
 
