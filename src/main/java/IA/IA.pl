5d89016f25498dd8a21b2be571905eab9f0719a6
Tanguy <tanguy.plaza@hotmail.fr>
2020-05-10 03:50:49 +0200

    fini carré is gagnant

--- src/main/java/IA/IA.pl
+++ src/main/java/IA/IA.pl
@@ -12,27 +12,27 @@ testLigne(L,Li,T) :-
     nth0(E,L,I),
     I=\=T,
     F is Li*4+1,
-    nth0(F,L,I),
-    I=\=T,
+    nth0(F,L,I1),
+    I1=\=T,
     G is Li*4+2,
-    nth0(G,L,I),
-    I=\=T,
+    nth0(G,L,I2),
+    I2=\=T,
     H is Li*4+3,
-    nth0(H,L,I),
-    I=\=T.
+    nth0(H,L,I3),
+    I3=\=T.
 
 testColonne(L,Co,T) :-
     nth0(Co,L,I),
     I=\=T,
     E is Co+4,
-    nth0(E,L,I),
-    I=\=T,
+    nth0(E,L,I1),
+    I1=\=T,
     F is Co+8,
-    nth0(F,L,I),
-    I=\=T,
+    nth0(F,L,I2),
+    I2=\=T,
     G is Co+12,
-    nth0(G,L,I),
-    I=\=T.
+    nth0(G,L,I3),
+    I3=\=T.
 
 
 testCarre(L,Li,Co,T):-
@@ -80,6 +80,7 @@ fonction(L,L2,Res):-
     testLigne(L,Li,T),
     testColonne(L,Co,T),
     testCarre(L,Li,Co,T),
+    testGagnant(L,Li,Co,T),
     Res = [Li,Co,T].
 
 testLigneGagnant(L,Li,Co,T, Ps,PsI):-
@@ -115,7 +116,90 @@ testColonneGagnant(L,Li,Co,T,Ps,PsI):-
     I =\= -1,
     select(I,Ps,PsI).
 
-isGagnant(L, Li, Co, Pion):-
-    testLigneGagnant(L,Li,Pion), !.
-isGagnant(L, Li, Co, Pion):-
-    testColonneGagnant(L,Li,Pion), !.
+testCarreGagnant(L,Li,Co,T,Res):-
+    Ps = [0,1,2,3],
+    E is Li mod 2,
+    E == 0,
+    F is Co mod 2,
+    F == 0,
+    G is (1+Li)*4+Co+1,
+    nth0(G,L,I),
+    I =\= -1,
+    select(I,Ps,PsI),
+    G1 is Li * 4 + Co + 1,
+    nth0(G1,L,I1),
+    I1 =\= -1,
+    select(I1,PsI,PsI1),
+    G2 is (1 + Li) * 4 + Co,
+    nth0(G2,L,I2),
+    I2 =\= -1,
+    select(I2,PsI1,PsI2),
+    nth0(0,PsI2,Res),!.
+
+testCarreGagnant(L,Li,Co,T,Res):-
+    Ps = [0,1,2,3],
+    E is Li mod 2,
+    E == 0,
+    F is Co mod 2,
+    F == 1,
+    G is (1+Li)*4+Co-1,
+    nth0(G,L,I),
+    I =\= -1,
+    select(I,Ps,PsI),
+    G1 is Li * 4 + Co - 1,
+    nth0(G1,L,I1),
+    I1 =\= -1,
+    select(I1,PsI,PsI1),
+    G2 is (1 + Li) * 4 + Co,
+    nth0(G2,L,I2),
+    I2 =\= -1,
+    select(I2,PsI1,PsI2),
+    nth0(0,PsI2,Res),!.
+
+testCarreGagnant(L,Li,Co,T,Res):-
+    Ps = [0,1,2,3],
+    E is Li mod 2,
+    E == 1,
+    F is Co mod 2,
+    F == 0,
+    G is (Li-1)*4+Co+1,
+    nth0(G,L,I),
+    I =\= -1,
+    select(I,Ps,PsI),
+    G1 is Li * 4 + Co + 1,
+    nth0(G1,L,I1),
+    I1 =\= -1,
+    select(I1,PsI,PsI1),
+    G2 is (Li -1) * 4 + Co,
+    nth0(G2,L,I2),
+    I2 =\= -1,
+    select(I2,PsI1,PsI2),
+    nth0(0,PsI2,Res),!.
+
+
+testCarreGagnant(L,Li,Co,T,Res):-
+    Ps = [0,1,2,3],
+    E is Li mod 2,
+    E == 1,
+    F is Co mod 2,
+    F == 1,
+    G is (Li-1)*4+Co-1,
+    nth0(G,L,I),
+    I =\= -1,
+    select(I,Ps,PsI),
+    G1 is Li * 4 + Co - 1,
+    nth0(G1,L,I1),
+    I1 =\= -1,
+    select(I1,PsI,PsI1),
+    G2 is (Li-1) * 4 + Co,
+    nth0(G2,L,I2),
+    I2 =\= -1,
+    select(I2,PsI1,PsI2),
+    nth0(0,PsI2,Res),!.
+
+testGagnant(L,Li,Co,T):-
+    testCarreGagnant(L,Li,Co,T,Res),!.
+testGagnant(L,Li,Co,T):-
+    testLignesGagnant(L,Li,Co,T,Res),!.
+testGagnant(L,Li,Co,T):-
+    testColonnesGagnant(L,Li,Co,T,Res),!.
