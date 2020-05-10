8908a4598e72fac82a515e0a57d08e259186fd3e
Tanguy <tanguy.plaza@hotmail.fr>
2020-05-10 18:15:05 +0200

    ALLO 4 MODULO 4 CA FAIT 0

--- src/main/java/IA/IA.pl
+++ src/main/java/IA/IA.pl
@@ -7,66 +7,81 @@ testCase(L,X) :-
     nth0(X,L,I),
     I<0.
 
-testLigne(L,Li,T) :-
+testLigne(L,Li,T,C) :-
     E is Li*4,
-    nth0(E,L,I),
-    I=\=T,
+    nth0(E,L,[Type,Coul]),
+    C =\= Coul ->
+        Type =\= T
+    ;
     F is Li*4+1,
-    nth0(F,L,I1),
-    I1=\=T,
+    nth0(F,L,[Type1,Coul1]),
+    C =\= Coul1 ->
+        Type1 =\= T
+    ;
     G is Li*4+2,
-    nth0(G,L,I2),
-    I2=\=T,
+    nth0(G,L,[Type2,Coul2]),
+    C =\= Coul2 ->
+        Type2 =\= T
+    ;
     H is Li*4+3,
-    nth0(H,L,I3),
-    I3=\=T.
+    nth0(H,L,[Type3,Coul3]),
+    C =\= Coul3 ->
+        Type3 =\= T.
 
-testColonne(L,Co,T) :-
-    nth0(Co,L,I),
-    I=\=T,
+testColonne(L,Co,T,C) :-
+    nth0(Co,L,[Type,Coul]),
+    Coul =\= C ->
+        Type =\= T
+    ;
     E is Co+4,
-    nth0(E,L,I1),
-    I1=\=T,
+nth0(E,L,[Type1,Coul1]),
+    Coul1 =\= C ->
+        Type1 =\= T
+    ;
     F is Co+8,
-    nth0(F,L,I2),
-    I2=\=T,
+    nth0(F,L,[Type2,Coul2]),
+    Coul2=\=C ->
+        Type2 =\= T
+    ;
     G is Co+12,
-    nth0(G,L,I3),
-    I3=\=T.
+    nth0(G,L,[Type3,Coul3]),
+    Coul3=\=C ->
+        Type3 =\= T.
 
 
-testCarre(L,Li,Co,T):-
+testCarre(L,Li,Co,T,C):-
     E is Li mod 2,
     E == 0,
     F is Co mod 2,
     F == 0,
     G is (1+Li)*4+Co+1,
-    nth0(G,L,I),
-    I=\=T,!.
+    nth0(G,L,[Type,Coul]),
+    Coul =\= C ->
+        Type =\= T,!.
 testCarre(L,Li,Co,T):-
     E is Li mod 2,
     E == 0,
     F is Co mod 2,
     F == 1,
     G is (1+Li)*4+Co-1,
-    nth0(G,L,I),
-    I=\=T,!.
+    Coul =\= C ->
+        Type =\= T,!.
 testCarre(L,Li,Co,T):-
     E is Li mod 2,
     E == 1,
     F is Co mod 2,
     F == 0,
     G is (Li-1)*4+Co+1,
-    nth0(G,L,I),
-    I=\=T,!.
+    Coul =\= C ->
+        Type =\= T,!.
 testCarre(L,Li,Co,T):-
     E is Li mod 2,
     E == 1,
     F is Co mod 2,
     F == 1,
     G is (Li-1)*4+Co-1,
-    nth0(G,L,I),
-    I=\=T,!.
+    Coul =\= C ->
+        Type =\= T,!.
 
 fonction(L,L2,Case,Gagnant,Ligne,Colonne,Piece):-
     (caseGagnante(L,Case,Li,Co,T) ->
@@ -98,21 +113,24 @@ fonction(L,L2,Case,Gagnant,Ligne,Colonne,Piece):-
     ;
     fonction(L,L2,CaseX,Gagnant,Ligne,Colonne,Piece),!).
 
-testLigneGagnant(L,Li,Co, Ps,PsI):-
+testLigneGagnant(L,Li,Co,Acc,Acc1):-
     E is Li * 4 + Co,
-    nth0(E,L,I),
-    I =\= -1,
-    select(I,Ps,PsI).
+    nth0(E,L,[Type,Coul]),
+    Type =\= -1,
+    \+member(Type,Acc),
+    append(Acc,Type,Acc1).
+    
 
 testLignesGagnant(L,Li,Co,T) :-
     Ps = [0,1,2,3],
     E is Co + 1 mod 4,
-    testLigneGagnant(L,Li,E,Ps,PsI),
+    testLigneGagnant(L,Li,E,Acc,Acc1),
     F is Co + 2 mod 4,
-    testLigneGagnant(L,Li,F,PsI,PsI1),
+    testLigneGagnant(L,Li,F,Acc1,Acc2),
     G is Co + 3 mod 4,
-    testLigneGagnant(L,Li,G,PsI1,PsI2),
-    nth0(0,PsI2,T).
+    testLigneGagnant(L,Li,G,Acc2,Acc3),
+    msort(Acc3,Sort),
+    select(T,[0,1,2,3],Acc3).
 
 
 testColonnesGagnant(L,Li,Co,T):-
